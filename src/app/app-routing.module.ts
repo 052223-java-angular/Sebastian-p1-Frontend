e6b06c89a19729ea544734de4662402766db38d3
Sebastian Shader <sebfumaster@aol.com>
2023-06-15 13:41:10 -0700

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LibrariesComponent } from './pages/libraries/libraries.component';
+import { SearchComponent } from './pages/search/search.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'libraries', component: LibrariesComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
