fec29277c00adba36c5d844ce5697ac547a04ed6
Sebastian Shader <sebfumaster@aol.com>
2023-06-16 01:50:36 -0700

    finish login & register forms (still need to submit)

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,18 +8,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   registerFormGroup!: FormGroup;
-
+  nonMatching: boolean = false;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    // 8-20 alphanumeric chars, no repeated _ or . chars, can't end or start with . or _
+    let usernameValidator: ValidatorFn = Validators.pattern("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$");
+    // at least 8 chars, must contain 1 number
+    let passwordValidator: ValidatorFn = Validators.pattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
     this.registerFormGroup = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', [Validators.required]],
-      confirmPassword: ['', [Validators.required]],
+      username: ['', [Validators.required, usernameValidator]],
+      password: ['', [Validators.required, passwordValidator]],
+      confirmPassword: ['', [Validators.required, passwordValidator]],
     });
   }
 
   submitForm(): void {
-    
+    if(this.registerFormGroup.get('password')?.value != this.registerFormGroup.get('confirmPassword')?.value) {
+      this.nonMatching = true;
+      return;
+    }
+    this.nonMatching = false;
   }
 }
